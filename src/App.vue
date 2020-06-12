71744a831a1eab761983442e6d53e659d661cfc1
fzsong3 <fzsong3@iflytek.com>
2020-06-12 15:41:18 +0800

    feat: 项目新建

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <FormTest />
+<!--    <Test />-->
+  </div>
+</template>
+
+<script>
+import FormTest from './components/FormTest.vue'
+// import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormTest,
+    // Test,
+  }
+}
+</script>
+
+<style>
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    height: 100%;
+    display: flex;
+  }
+</style>
