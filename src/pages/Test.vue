0c6e50802f2ee2ec290956bddf97a24b8f7d7101
fzsong3 <fzsong3@iflytek.com>
2020-09-04 16:55:46 +0800

    fix: format code

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -5,20 +5,18 @@
 -->
 <template>
   <div class="test">
-    <TestChild/>
-<!--    <div v-show="showFlag" v-if="ifFlag">-->
-<!--&lt;!&ndash;      <div class="box" v-wrap-title >{{ text | testFormat }}</div>&ndash;&gt;-->
-<!--      <TestChild/>-->
-<!--    </div>-->
-<!--    <el-button @click="showFlag = !showFlag">show显示、关闭</el-button>-->
-<!--    <el-button @click="ifFlag = !ifFlag">if显示、关闭</el-button>-->
+    <TestChild />
+    <!--    <div v-show="showFlag" v-if="ifFlag">-->
+    <!--&lt;!&ndash;      <div class="box" v-wrap-title >{{ text | testFormat }}</div>&ndash;&gt;-->
+    <!--      <TestChild/>-->
+    <!--    </div>-->
+    <!--    <el-button @click="showFlag = !showFlag">show显示、关闭</el-button>-->
+    <!--    <el-button @click="ifFlag = !ifFlag">if显示、关闭</el-button>-->
   </div>
 </template>
 
 <script>
-
-import TestChild from "./TestChild";
-
+import TestChild from './TestChild'
 export default {
   name: 'Test',
   components: { TestChild },
@@ -34,24 +32,27 @@ export default {
       return value + '13245'
     },
   },
-  mounted () {
-  },
-  beforeDestroy () {
-  },
+  mounted () { },
+  beforeDestroy () { },
   methods: {
-  }
+    diyTest (pram2, paramArr) {
+      console.log(pram2)
+      console.log(paramArr)
+      return paramArr + pram2
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-  .test {
-    height: 100%;
-    width: 100%;
-    .box {
-      width: 200px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
+.test {
+  height: 100%;
+  width: 100%;
+  .box {
+    width: 200px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
+}
 </style>
