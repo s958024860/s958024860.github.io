ea19871866f4bce653dda175acbf6acba2eeb4b1
fzsong3 <fzsong3@iflytek.com>
2020-09-10 08:45:30 +0800

    feat: 地图新增

---
+++ src/pages/AnHuiMap.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: fzsong3
+ * @Date: 2020/9/7 11:48
+ * @Description:
+-->
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+  export default {
+    name: 'AnHuiMap'
+  }
+</script>
+
+<style scoped>
+
+</style>
